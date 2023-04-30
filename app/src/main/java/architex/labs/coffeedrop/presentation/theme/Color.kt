ea8f1074ad2c5eca23f7dd8c22ceb27a10bf9909
Xero kun <elo48858@gmail.com>
2023-04-30 14:57:58 +0100

    `🌸SETUP` Initial Project Commit

---
+++ app/src/main/java/architex/labs/coffeedrop/presentation/theme/Color.kt
@@ -0,0 +1,13 @@
+package architex.labs.coffeedrop.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+/** [NEUTRALS] */
+val Neutrals400 = Color(0xFF090C11)
+val Neutrals300 = Color(0xFF131820)
+val Neutrals200 = Color(0xFF9BA6B3)
+val Neutrals100 = Color(0xFFF6F6F7)
+
+/** [PRIMARY & ACCENT] */
+val Primary = Color(0xFFED8B60)
+val Accent = Color(0xFF462518)
