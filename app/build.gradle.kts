757d4e571014bdffc33bef40ae252ff874f96381
Xero kun <elo48858@gmail.com>
2023-04-30 17:33:17 +0100

    `🌱UPDATE` Implemented Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     val coreVersion = "1.10.0"
     val lifecycleVersion = "2.6.1"
     val activityComposeVersion = "1.7.1"
+    val splashScreenVersion = "1.0.1"
 
     implementation ("androidx.core:core-ktx:$coreVersion")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
@@ -64,6 +65,8 @@ dependencies {
     implementation ("androidx.compose.ui:ui-graphics")
     implementation ("androidx.compose.ui:ui-tooling-preview")
     implementation ("androidx.compose.material3:material3")
+    implementation ("androidx.core:core-splashscreen:$splashScreenVersion")
+
     testImplementation ("junit:junit:4.13.2")
     androidTestImplementation ("androidx.test.ext:junit:1.1.5")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/architex/labs/coffeedrop/presentation/MainActivity.kt
+++ app/src/main/java/architex/labs/coffeedrop/presentation/MainActivity.kt
@@ -1,4 +1,19 @@
-package architex.labs.coffeedrop
+/**
+* Copyright (C) [2023 - Present] - Xero
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package architex.labs.coffeedrop.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,10 +25,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import architex.labs.coffeedrop.presentation.theme.CoffeeDropTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
+		installSplashScreen()
 		super.onCreate(savedInstanceState)
 		setContent {
 			CoffeeDropTheme {
