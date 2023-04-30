851c619df04ef732e58f03f7f1e824569078c587
Xero kun <elo48858@gmail.com>
2023-04-30 20:45:22 +0100

    `🌱UPDATE` Uploaded App Fonts

--- app/src/main/java/architex/labs/coffeedrop/presentation/screens/HomeScreen.kt
+++ app/src/main/java/architex/labs/coffeedrop/presentation/screens/HomeScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -39,12 +38,7 @@ fun HomeScreen(
 		},
 		content = { contentPadding ->
 			LazyColumn(modifier = Modifier.padding(contentPadding)) {
-				item {
-					Text(
-						text = "String",
-						modifier = Modifier.padding(horizontal = 16.dp)
-					)
-				}
+				item {}
 			}
 		}
 	)
