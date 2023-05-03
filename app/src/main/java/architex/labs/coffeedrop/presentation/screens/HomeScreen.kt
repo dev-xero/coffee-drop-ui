30a5e9093f128695192e6206955ab5a31c5b2584
Xero kun <elo48858@gmail.com>
2023-05-03 22:19:28 +0100

    `🍜FIX` Changed Font Styles for Leading Texts

--- app/src/main/java/architex/labs/coffeedrop/presentation/screens/HomeScreen.kt
+++ app/src/main/java/architex/labs/coffeedrop/presentation/screens/HomeScreen.kt
@@ -27,13 +27,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import architex.labs.coffeedrop.R
 import architex.labs.coffeedrop.presentation.components.AppBar
 import architex.labs.coffeedrop.presentation.components.CoffeeFilterBar
 import architex.labs.coffeedrop.presentation.components.SearchBar
-import architex.labs.coffeedrop.presentation.theme.CircularStd
 import architex.labs.coffeedrop.presentation.theme.Neutrals100
 import architex.labs.coffeedrop.presentation.theme.Neutrals200
 import architex.labs.coffeedrop.presentation.theme.Neutrals400
@@ -60,17 +58,13 @@ fun HomeScreen(
 					) {
 						Text(
 							text = stringResource(id = R.string.headline_1),
-							style = MaterialTheme.typography.headlineMedium,
-							color = Neutrals200,
-							fontFamily = CircularStd,
-							fontWeight = FontWeight.Bold
+							style = MaterialTheme.typography.displayMedium,
+							color = Neutrals200
 						)
 						Text(
 							text = stringResource(id = R.string.headline_2),
-							style = MaterialTheme.typography.headlineLarge,
-							color = Neutrals100,
-							fontFamily = CircularStd,
-							fontWeight = FontWeight.Bold
+							style = MaterialTheme.typography.displayLarge,
+							color = Neutrals100
 						)
 					}
 
