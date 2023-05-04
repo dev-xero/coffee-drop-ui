6ff4ffebe7ea8a92151d17dc45221f81d433d433
Xero kun <elo48858@gmail.com>
2023-05-04 22:59:50 +0100

    `🌱UPDATE` Added `10` More Entries to Coffee Data Repository

--- app/src/main/java/architex/labs/coffeedrop/data/repository/CoffeeListImpl.kt
+++ app/src/main/java/architex/labs/coffeedrop/data/repository/CoffeeListImpl.kt
@@ -30,7 +30,7 @@ class CoffeeListImpl : CoffeeList {
 				imageResID = R.drawable.image_1,
 				name = R.string.cappuccino,
 				description = R.string.cappuccino_description,
-				variant = "With Oak Milk",
+				variant = "With Oat Milk",
 				coffeeType = CoffeeType.Cappuccino,
 				price = 4.20,
 				rating = 4.6,
@@ -111,7 +111,7 @@ class CoffeeListImpl : CoffeeList {
 				variant = "With Skim Milk",
 				coffeeType = CoffeeType.Cappuccino,
 				price = 4.45,
-				rating = 4.1,
+				rating = 4.5,
 				reviews = 3270,
 				roastingLevel = RoastingLevel.HighRoasted,
 				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
@@ -155,5 +155,135 @@ class CoffeeListImpl : CoffeeList {
 				roastingLevel = RoastingLevel.MediumRoasted,
 				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
 			),
+
+			Coffee(
+				imageResID = R.drawable.image_11,
+				name = R.string.cortado,
+				description = R.string.cortado_description,
+				variant = "With Oat Milk",
+				coffeeType = CoffeeType.Cortado,
+				price = 5.12,
+				rating = 4.8,
+				reviews = 33001,
+				roastingLevel = RoastingLevel.HighRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_12,
+				name = R.string.latte,
+				description = R.string.latte_description,
+				variant = "With Soy Milk",
+				coffeeType = CoffeeType.Latte,
+				price = 4.90,
+				rating = 4.6,
+				reviews = 7020,
+				roastingLevel = RoastingLevel.LowRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_13,
+				name = R.string.cappuccino,
+				description = R.string.cappuccino_description,
+				variant = "With Oat Milk",
+				coffeeType = CoffeeType.Cappuccino,
+				price = 4.90,
+				rating = 4.7,
+				reviews = 62010,
+				roastingLevel = RoastingLevel.HighRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Large)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_14,
+				name = R.string.espresso,
+				description = R.string.espresso_description,
+				variant = "With Skim Milk",
+				coffeeType = CoffeeType.Espresso,
+				price = 4.40,
+				rating = 4.8,
+				reviews = 18231,
+				roastingLevel = RoastingLevel.MediumRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_15,
+				name = R.string.latte,
+				description = R.string.latte_description,
+				variant = "With Almond Milk",
+				coffeeType = CoffeeType.Latte,
+				price = 5.00,
+				rating = 4.9,
+				reviews = 77025,
+				roastingLevel = RoastingLevel.HighRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_16,
+				name = R.string.mocha,
+				description = R.string.mocha_description,
+				variant = "With Soy Milk",
+				coffeeType = CoffeeType.Mocha,
+				price = 4.75,
+				rating = 4.8,
+				reviews = 10030,
+				roastingLevel = RoastingLevel.LowRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_17,
+				name = R.string.cortado,
+				description = R.string.cortado_description,
+				variant = "With Skim Milk",
+				coffeeType = CoffeeType.Cortado,
+				price = 5.20,
+				rating = 4.9,
+				reviews = 98160,
+				roastingLevel = RoastingLevel.HighRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_18,
+				name = R.string.double_espresso,
+				description = R.string.double_espresso_description,
+				variant = "With Oat Milk",
+				coffeeType = CoffeeType.DoubleEspresso,
+				price = 4.10,
+				rating = 4.4,
+				reviews = 53990,
+				roastingLevel = RoastingLevel.LowRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_19,
+				name = R.string.double_espresso,
+				description = R.string.double_espresso_description,
+				variant = "With Coconut Milk",
+				coffeeType = CoffeeType.DoubleEspresso,
+				price = 4.20,
+				rating = 4.7,
+				reviews = 8100,
+				roastingLevel = RoastingLevel.MediumRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
+			),
+
+			Coffee(
+				imageResID = R.drawable.image_20,
+				name = R.string.mocha,
+				description = R.string.mocha_description,
+				variant = "With Coconut Milk",
+				coffeeType = CoffeeType.Mocha,
+				price = 4.80,
+				rating = 4.8,
+				reviews = 32021,
+				roastingLevel = RoastingLevel.MediumRoasted,
+				coffeeSize = listOf(CoffeeSize.Small, CoffeeSize.Medium, CoffeeSize.Large)
+			)
 		)
 }
